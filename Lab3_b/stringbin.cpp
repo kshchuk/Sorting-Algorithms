a923f920879c31daaa6a163ad5537d7dae36fbab
Yaroslav <98614059+kshchuk@users.noreply.github.com>
2022-04-19 01:13:47 +0300

    Add files via upload

---
+++ Lab3_b/stringbin.cpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <fstream>
+
+
+void writeStringToBinFile(std::string& str, std::fstream& of)
+{
+	size_t strLen = str.length() + 1;
+	of.write((char*)&strLen, sizeof(size_t));
+	of.write(str.data(), strLen);
+}
+
+void readStringFromBinFile(std::string& str, std::ifstream& in)
+{
+	size_t strLen;
+	in.read((char*)&strLen, sizeof(size_t));
+
+	char* buf = new char[strLen];
+	in.read(buf, strLen);
+	str = std::string(buf);
+
+	delete[] buf;
+}
