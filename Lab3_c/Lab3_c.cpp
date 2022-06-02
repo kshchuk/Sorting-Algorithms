e194b2bee499c17b683ae09ffbe0b0335b8627ce
kshchuk <kshchuk@gmail.com>
2022-06-03 01:52:21 +0300

    include error fixed

--- Lab3_c/Lab3_c.cpp
+++ Lab3_c/Lab3_c.cpp
@@ -1,6 +1,43 @@
-﻿
+﻿#include "MySorts.hpp"
 
-// 12. (**) Реалізувати алгоритм сортування introsort
+
+// 10. Реалізувати алгоритм сортування heap sort
+
+
+template <typename T>
+void heapify(T arr[], int n, int i)
+{
+    int largest = i;
+
+    int l = 2 * i + 1;
+
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+
+        heapify<T>(arr, n, largest);
+    }
+}
+
+template<typename T>
+void heapSort(T arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+
+        std::swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
 
 
 
