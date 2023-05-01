a006bd545c5232d1e627dbae9ce48e866e61ba09
kshchuk <kshchuk@gmail.com>
2023-05-01 13:02:39 +0300

    Added unit tests. Some doesn't pass

--- Lab3_c/Lab3_c.cpp
+++ Lab3_c/Lab3_c.cpp
@@ -1,124 +1,22 @@
-﻿#include "sort"
-
-#include <random>
-
-int* randomArr(size_t size) {
-    std::random_device rd;
-    std::mt19937 mersenne(rd());
-
-    int* arr = new int[size];
-    for (size_t i = 0; i < size; i++)
-        arr[i] = mersenne() % size;
-    return arr;
-}
-
-
-#if __has_include(<benchmark/benchmark.h>)
-#pragma comment ( lib, "Shlwapi.lib" )
-#include <benchmark/benchmark.h>                // https://github.com/google/benchmark
-
-static void BM_HeapSort(benchmark::State& state)
-{
-    std::random_device rd;
-    std::mt19937 mersenne(rd());
-
-    int* arr = randomArr(state.range(0));
-    
-    for (auto _ : state) {
-        heapSort(arr, state.range(0));
-    }
-    delete[] arr;
-}
-
-
-static void BM_QuickSort_MT(benchmark::State& state)
-{
-    std::random_device rd;
-    std::mt19937 mersenne(rd());
-
-    int threadsNum = std::thread::hardware_concurrency();
-    int* arr = randomArr(state.range(0));
-
-    for (auto _ : state) {
-        QuickSortMT(arr, state.range(0), threadsNum);
-    }
-    delete[] arr;
-
-}
-
-static void BM_MergeSort_MT(benchmark::State& state)
-{
-    std::random_device rd;
-    std::mt19937 mersenne(rd());
-
-    int* arr = randomArr(state.range(0));
-    int threadsNum = std::thread::hardware_concurrency();
-
-    for (auto _ : state) {
-        mergeSortMT(arr, state.range(0), threadsNum);
-    }
-    delete[] arr;
-
-}
-
-#include "MySorts.hpp"
-
-
-int comparator(const void* p1, const void* p2) {
-    int l = *(const int*)p1;
-    int r = *(const int*)p2;
-    return (l - r);
-}
-
-static void QuickSort_BM(benchmark::State& state)
-{
-    int* arr = randomArr(state.range(0));
-
-    for (auto _ : state) {
-        qsort(arr, state.range(0), sizeof(int), comparator);
-    }
-    delete[] arr;
-}
-
-
-static void MergeSort_BM(benchmark::State& state)
-{
-    int* arr1 = randomArr(state.range(0));
-    int* arr2 = new int[state.range(0)];
-
-    for (auto _ : state) {
-        MergeSort(arr1, arr2, state.range(0));
-    }
-    delete[] arr1, arr2;
-}
-
-static void LibrarySort_BM(benchmark::State& state)
-{
-    int* arr1 = randomArr(state.range(0));
-
-    for (auto _ : state) {
-        std::sort(arr1, arr1 + state.range(0));
-    }
-    delete[] arr1;
-}
+﻿//#include "sort"
+//
+//#include <random>
+//
+//int* randomArr(size_t size) {
+//    std::random_device rd;
+//    std::mt19937 mersenne(rd());
+//
+//    int* arr = new int[size];
+//    for (size_t i = 0; i < size; i++)
+//        arr[i] = mersenne() % size;
+//    return arr;
+//}
+//
+//
 
 
 
-
-BENCHMARK(BM_HeapSort)->Unit(benchmark::kMicrosecond)->RangeMultiplier(10)->Range(10, 10000000)->Iterations(1);
-BENCHMARK(BM_QuickSort_MT)->Unit(benchmark::kMicrosecond)->RangeMultiplier(10)->Range(10, 100000000)->Iterations(1);
-BENCHMARK(QuickSort_BM)->Unit(benchmark::kMicrosecond)->RangeMultiplier(10)->Range(10, 100000000)->Iterations(1);
-BENCHMARK(BM_MergeSort_MT)->Unit(benchmark::kMicrosecond)->RangeMultiplier(10)->Range(10, 100000000)->Iterations(1);
-BENCHMARK(MergeSort_BM)->Unit(benchmark::kMicrosecond)->RangeMultiplier(10)->Range(10, 100000000)->Iterations(1);
-BENCHMARK(LibrarySort_BM)->Unit(benchmark::kMicrosecond)->RangeMultiplier(10)->Range(10, 10000000)->Iterations(1);
-
-
-
-
-BENCHMARK_MAIN();
-
-
-#else
+//#else
 
 
 
@@ -185,44 +83,15 @@ BENCHMARK_MAIN();
 //LibrarySort_BM / 1000000 / iterations : 1         70601 us        62500 us            1
 //LibrarySort_BM / 10000000 / iterations : 1       816895 us       828125 us            1
 
+#include <iostream>
+#include "gtest/gtest.h"
 
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Heap sort\n";
-
-    int* arr = randomArr(10);
-    heapSort(arr, 10);
-    //QuickSortMT(arr, 10, std::thread::hardware_concurrency());
-    //mergeSortMT(arr, 10, std::thread::hardware_concurrency());
-
-	for (int i = 0; i < 10; i++)
-		std::cout << arr[i] << '\n';
-
-    std::cout << "\n\n";
-    std::cout << "QuickSort multithreaded\n";
-
-
-    arr = randomArr(10);
-
-    QuickSortMT(arr, 10, std::thread::hardware_concurrency());
-
-    for (int i = 0; i < 10; i++)
-        std::cout << arr[i] << '\n';
-
-    std::cout << "\n\n";
-    std::cout << "MergeSort multithreaded\n";
-
-    arr = randomArr(10);
-
-    mergeSortMT(arr, 10, std::thread::hardware_concurrency());
-
-
-    for (int i = 0; i < 10; i++)
-        std::cout << arr[i] << '\n';
-
-    std::cout << "\n\n";
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 
-#endif
+//#endif
 
