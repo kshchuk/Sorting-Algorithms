a3d9a7aaf67821ac9ab3ddfa68e5e321853501ea
kshchuk <kshchuk@gmail.com>
2023-06-15 01:16:53 +0300

    Added benchmarks

--- Lab3_c/Lab3_c.cpp
+++ Lab3_c/Lab3_c.cpp
@@ -4,7 +4,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 
 #include "doctest.h"
+#include <benchmark/benchmark.h>
+
 #include "tests.hpp"
+#include "benchmarks.hpp"
 
 
 
@@ -19,6 +22,22 @@ int main(int argc, char* argv[])
     if (context.shouldExit()) {
         return res;
     }
+
+    REGISTER_BENCHMARK(int, InsertionSort);
+    REGISTER_BENCHMARK(int, QuickSort);
+    REGISTER_BENCHMARK(int, MergeSort);
+    REGISTER_BENCHMARK(int, HeapSort);
+    REGISTER_BENCHMARK(int, BubbleSort);
+    REGISTER_BENCHMARK(int, SelectionSort);
+    REGISTER_BENCHMARK(int, ShellSort);
+    REGISTER_BENCHMARK(int, RadixSort);
+    REGISTER_BENCHMARK(int, CountingSort);
+    REGISTER_BENCHMARK(int, MultiThreadedQuickSort);
+    REGISTER_BENCHMARK(int, MultiThreadedMergeSort);
+    REGISTER_BENCHMARK(int, MultiThreadedHeapSort);
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
 }
 
 
